de1b727f8f0581eec3417996c50e9571b726acab
Stt3ve <106826334+Stt3ve@users.noreply.github.com>
2022-08-16 08:24:42 -0300

    Add files via upload

---
+++ C/negativos/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int N, nN, vet[10];
+
+    printf("Quantos numeros voce vai digitar? ");
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++){
+        printf("Digite um numero: ");
+        scanf("%d", &vet[i]);
+    }
+
+    printf("\nNUMEROS NEGATIVOS: \n");
+
+    for(int i = 0; i < N; i++){
+        if(vet[i] < 0){
+            printf("%d\n", vet[i]);
+        }
+    }
+
+    return 0;
+}
