dcb697d0526196e1711c0737410441d84ea0a3fe
Stt3ve <106826334+Stt3ve@users.noreply.github.com>
2022-09-02 11:25:56 -0300

    Add files via upload

---
+++ c#/CalculaIMC/CalculaIMC/Program.cs
@@ -0,0 +1,45 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o peso em kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a altura em metros: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double IMC = peso / (altura * altura);
+
+            if (IMC < 20)
+            {
+                Console.WriteLine("\nIMC = " + IMC + " -> Abaixo do peso." );
+            }
+            else if ((IMC >= 20) && (IMC <= 24))
+            {
+                Console.WriteLine("\nIMc =" + IMC + " -> Normal.");
+            }
+            else if ((IMC >= 25) && (IMC <= 29))
+            {
+                Console.WriteLine("\nIMc =" + IMC + " -> Acima do peso.");
+            }
+            else if ((IMC >= 30) && (IMC <= 34))
+            {
+                Console.WriteLine("\nIMc =" + IMC + " -> Obesidade.");
+            }
+            else
+            {
+                Console.WriteLine("\nIMc =" + IMC + " -> Obesidade extrema.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
